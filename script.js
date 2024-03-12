77834409fc31fe1c071092cc9860d2ffedb6379a
Hori2003 <daisy.chen78@myhunter.cuny.edu>
2024-03-12 14:40:35 -0400

    add row/col

--- script.js
+++ script.js
@@ -5,41 +5,37 @@ let colorSelected;
 
 // Add a row
 function addR() {
-    alert("Clicked Add Row"); // Replace this line with your code.
+    // alert("Clicked Add Row"); // Replace this line with your code.
+    let table = document.getElementById("grid");
+    let row = table.insertRow(numRows);
+
+    if (numRows == 0){
+        table.rows[table.rows.length -1].insertCell(0);
+        numCols++;
+    }
+    
+    else{
+    for(var i = 0; i < numCols; i++){
+    table.rows[table.rows.length -1].insertCell(i);
+    }
+}
+numRows++;
 }
 
 // Add a column
 function addC() {
-    alert("Clicked Add Col"); // Replace this line with your code.
-}
-
-// Remove a row
-function removeR() {
-    alert("Clicked Remove Row"); // Replace this line with your code.
-}
-
-// Remove a column
-function removeC() {
-    alert("Clicked Remove Col"); // Replace this line with your code.
-}
-
-// Set global variable for selected color
-function selectColor(){
-    colorSelected = document.getElementById("selectedColorId").value;
-    console.log(colorSelected);
-}
-
-// Fill all uncolored cells
-function fillU(){
-    alert("Clicked Fill All Uncolored"); // Replace this line with your code.
-}
-
-// Fill all cells
-function fillAll(){
-    alert("Clicked Fill All"); // Replace this line with your code.
-}
-
-// Clear all cells
-function clearAll(){
-    alert("Clicked Clear All"); // Replace this line with your code.
+    // alert("Clicked Add Col"); // Replace this line with your code.
+        // alert("Clicked Add Row"); // Replace this line with your code.
+        let table = document.getElementById("grid");
+    
+        if (numRows == 0){
+            table.insertRow(numRows);
+            numRows++
+        }
+        
+        for(var i = 0; i < numRows; i++){
+        table.rows[i].insertCell(numCols);
+        }
+    
+    numCols++;
 }
