507e1db6be947f016ca72984d4859f8a8d028aee
Hori2003 <daisy.chen78@myhunter.cuny.edu>
2024-03-12 14:51:32 -0400

    Clear

--- script.js
+++ script.js
@@ -3,22 +3,18 @@ let numRows = 0;
 let numCols = 0;
 let colorSelected; 
 
-// Fill all uncolored cells
-function fillU(){
-    // alert("Clicked Fill All Uncolored"); // Replace this line with your code.
-   // document.getElementsByTagName("td");
-    for(let i = 0; i < document.getElementsByTagName("td").length; i++){
-        if(document.getElementsByTagName("td")[i].style.backgroundColor.valueOf() != "red" && document.getElementsByTagName("td")[i].style.backgroundColor.valueOf() != "blue" && document.getElementsByTagName("td")[i].style.backgroundColor.valueOf() != "green" && document.getElementsByTagName("td")[i].style.backgroundColor.valueOf() != "yellow" ){
-            document.getElementsByTagName("td")[i].style.backgroundColor = colorSelected;
-        }
+
+
+// Clear all cells
+function clearAll(){
+    // alert("Clicked Clear All"); // Replace this line with your code.
+    if (numRows === 0) {
+        alert("There is nothing to clear!");
     }
+    while (numRows > 0) {
+        grid.deleteRow(-1);
+        numRows--;
+    }
+    numCols = 0;
+    numRows = 0;
 }
-
-// Fill all cells
-function fillAll(){
-    // alert("Clicked Fill All"); // Replace this line with your code.
-    for(let i = 0; i < document.getElementsByTagName("td").length; i++){
-        document.getElementsByTagName("td")[i].style.backgroundColor = colorSelected;
-        }
-    }
-
