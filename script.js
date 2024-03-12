d128315914e31779700ee393c25d3676e3a6721b
Hori2003 <daisy.chen78@myhunter.cuny.edu>
2024-03-12 14:45:46 -0400

    s

--- script.js
+++ script.js
@@ -4,32 +4,9 @@ let numCols = 0;
 let colorSelected; 
 
 
-
-// Remove a row
-function removeR() {
-    // alert("Clicked Remove Row"); // Replace this line with your code.
-    let table = document.getElementById("grid");
-    if(numRows > 0){
-        table.deleteRow(table.rows.length - 1)
-        numRows--;
-        if (numRows == 0) {
-            numCols = 0;
-        }
-    }
+// Set global variable for selected color
+function selectColor(){
+    colorSelected = document.getElementById("selectedColorId").value;
+    console.log(colorSelected);
 }
 
-// Remove a column
-function removeC() {
-    // alert("Clicked Remove Col"); // Replace this line with your code.
-    let table = document.getElementById("grid");
-    if(numCols > 0){
-        for(var i = 0; i < table.rows.length; i++){
-            table.rows[i].deleteCell(-1)
-        }
-        numCols--;
-        while (numCols == 0) {
-            table.deleteRow(0);
-            numRows--;
-        }
-    }
-}
