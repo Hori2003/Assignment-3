4c4a589de7e815a0c6dbd7eedcb708d57c428ef0
Hori2003 <daisy.chen78@myhunter.cuny.edu>
2024-03-12 14:48:46 -0400

    Fill

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ let numRows = 0;
 let numCols = 0;
 let colorSelected; 
 
-
-// Set global variable for selected color
-function selectColor(){
-    colorSelected = document.getElementById("selectedColorId").value;
-    console.log(colorSelected);
+// Fill all uncolored cells
+function fillU(){
+    // alert("Clicked Fill All Uncolored"); // Replace this line with your code.
+   // document.getElementsByTagName("td");
+    for(let i = 0; i < document.getElementsByTagName("td").length; i++){
+        if(document.getElementsByTagName("td")[i].style.backgroundColor.valueOf() != "red" && document.getElementsByTagName("td")[i].style.backgroundColor.valueOf() != "blue" && document.getElementsByTagName("td")[i].style.backgroundColor.valueOf() != "green" && document.getElementsByTagName("td")[i].style.backgroundColor.valueOf() != "yellow" ){
+            document.getElementsByTagName("td")[i].style.backgroundColor = colorSelected;
+        }
+    }
 }
+
+// Fill all cells
+function fillAll(){
+    // alert("Clicked Fill All"); // Replace this line with your code.
+    for(let i = 0; i < document.getElementsByTagName("td").length; i++){
+        document.getElementsByTagName("td")[i].style.backgroundColor = colorSelected;
+        }
+    }
 
